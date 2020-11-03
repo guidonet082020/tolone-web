8cfdb7ad8addfc0ed3a86c12dd2c7141a0d707f7
Gomez Tomas Gonzalo <talk.tomas@hotmail.com>
2020-11-02 21:11:18 -0300

    PHP logic implements

--- php/database.php
+++ php/database.php
@@ -1,3 +1,37 @@
 <?php
-    $connection = mysqli_connect('192.168.0.61','ad_tolone-web','a2PEit6hg7G7','tolone-web-db');
+    //$connection = mysqli_connect('192.168.0.61','ad_tolone-web','a2PEit6hg7G7','tolone-web-db;
+
+    class DB{
+
+        private $host;
+        private $db;
+        private $user;
+        private $password;
+        private $charset;
+
+        public function __construct(){
+            $this->host = '192.168.0.61';
+            $this->db = 'tolone-web-db';
+            $this->user = 'ad_tolone-web';
+            $this->password = 'a2PEit6hg7G7';
+            $this->charset = 'utf8mb4';
+        }
+
+        function connect(){
+
+            $conn = new mysqli($this->host, $this->user, $this->password, $this->db);
+
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . $conn->connect_error());
+            }
+
+            //echo "Connected successfully";
+            //$conn->close();
+
+            return $conn;
+        }
+
+    }
+    
 ?>
